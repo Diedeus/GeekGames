31be7ef77ed2f5549569bcfb0051ba6b7f8dc314
= <palerm39@gmail.com>
2023-05-19 10:49:53 +0200

    1er commit du 19 mai

--- Jeux/Quizz/Quizz.php
+++ Jeux/Quizz/Quizz.php
@@ -20,12 +20,16 @@ require('../../../geekgames/Registration/config.php');
 ?>
 
 <?php
-$query = "SELECT intitule_question, Reponse_A, Reponse_B, Reponse_C, Reponse_D, Difficulte_question FROM jeu_quizz ORDER BY RAND() LIMIT 1";
+$query = "SELECT intitule_question, Reponse_A, Reponse_B, Reponse_C, Reponse_D, Reponse_Quizz, Difficulte_question FROM jeu_quizz ORDER BY RAND() LIMIT 1";
 $result = $conn->query($query);
+session_save_path("../../tmp");
+session_start();
+
 
 if ($result->num_rows > 0) {
     // Générer et afficher le quiz
     $question = $result->fetch_assoc();
+    $_SESSION['Reponse_Quizz'] = $question['Reponse_Quizz'];
     displayQuestion($question);
     } else {
     echo "Aucune question trouvée dans la base de données.";
@@ -34,7 +38,7 @@ if ($result->num_rows > 0) {
 // Fonction pour afficher une question
 function displayQuestion($question) {
     echo "Question: " . $question["intitule_question"] . "<br>";
-    echo "A) " . $question["Reponse_A"] . "<br>";
+    echo "<input type='submit' value='A) " . $question["Reponse_A"] . "'><br>";
     echo "B) " . $question["Reponse_B"] . "<br>";
     echo "C) " . $question["Reponse_C"] . "<br>";
     echo "D) " . $question["Reponse_D"] . "<br>";
