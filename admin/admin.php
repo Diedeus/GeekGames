29ef02006cc227d08cb866c32ead71da8d4e10b2
=AlberickDupont <alberick.dupont@gmail.com>
2023-05-19 15:32:01 +0200

    front de la page admin pour le quizz

--- admin/admin.php
+++ admin/admin.php
@@ -8,13 +8,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
   <title>GeekGames - Home</title>
-  <link rel="stylesheet" href="../styles/style.css">
+  <link rel="stylesheet" href="admin.css">
   <script src="../script/script.js" defer></script>
 
 
 </head>
 
 <body>
+<nav class="home">
+            <ul>
+                <li><a href="../index.php">MINI JEUX</a></li>
+                <li><a href="#">CREDITS</a></li>
+                <li><a href="#">NOUS CONTACTER</a></li>
+            </ul>
+        </nav>
+        <div class="clear"></div>
+        <span class="barre"></span>
+
+  <section class="container_formquiz">
+<div class="formleft">
   <?php
   // VERIFICATION ADMIN OU UTILISATEUR
 
@@ -33,84 +45,118 @@
 
   <!-- FORMULAIRE CREATION DE QUESTION  -->
   <form class="box" action="" method="post" name="Jeu_Quizz">
-    <h2 class="box-title">CREATION DES QUESTIONS DU QUIZZ</h2>
+    <h2 class="box-title">Création de question QUIZZ</h2>
     <input type="text" class="box-input" name="Intitule_question" placeholder="Qu'elle est la question">
-    <input type="text" class="box-input" name="Reponse_A" placeholder="Reponse A">
-    <input type="text" class="box-input" name="Reponse_B" placeholder="Reponse B">
-    <input type="text" class="box-input" name="Reponse_C" placeholder="Reponse C">
-    <input type="text" class="box-input" name="Reponse_D" placeholder="Reponse D">
-    <input type="text" class="box-input" name="Reponse_Quizz" placeholder="Reponse">
-    <input type="text" class="box-input" name="Difficulte_question" placeholder="Qu'elle est la difficulte">
-    <input type="submit" value="Creer" name="submit" class="connecter">
+    <input type="text" class="box-reponse" name="Reponse_A" placeholder="Reponse A">
+    <input type="text" class="box-reponse" name="Reponse_B" placeholder="Reponse B">
+    <input type="text" class="box-reponse" name="Reponse_C" placeholder="Reponse C">
+    <input type="text" class="box-reponse" name="Reponse_D" placeholder="Reponse D">
+    <input type="text" class="box-reponse" name="Reponse_Quizz" placeholder="Reponse">
+    <input type="text" class="box-reponse" name="Difficulte_question" placeholder="Qu'elle est la difficulte">
+    <input class="update-delete" type="submit" value="Creer la question" name="submit" class="connecter">
     <?php if (! empty($message)) { ?>
     <p class="errorMessage"><?php echo $message; ?></p>
     <?php } ?>
   </form>
-  <img src="asset/logogeekgame.png" alt="">
 
+  
 
-  <?php 
-// ON VA CHERCHER LES LOGS DE LA DB DANS LE FICHIER CONFIG.PHP
+<?php
+  // ON VA CHERCHER LES LOGS DE LA DB DANS LE FICHIER CONFIG.PHP
 require('../Registration/config.php');
-
-// On vérifie si les variables  sont définies dans la requête en utilisant la fonction "isset()"
-   if(isset($_REQUEST["Intitule_question"],$_REQUEST["Reponse_A"],$_REQUEST["Reponse_B"],$_REQUEST["Reponse_C"],$_REQUEST["Reponse_D"],$_REQUEST["Reponse_Quizz"],$_REQUEST["Difficulte_question"])){
-    
-
-    //! On supprime les caractères d'échappement des variables avec la fonction "stripslashes() et on empêche les injections SQL avec la fonction "mysqli_real_escape_string"
-    $Intitule_question = stripslashes($_REQUEST["Intitule_question"]);
-    $Intitule_question = mysqli_real_escape_string($conn, $Intitule_question);
-
-    $Reponse_A = stripslashes($_REQUEST["Reponse_A"]);
-    $Reponse_A = mysqli_real_escape_string($conn, $Reponse_A);
-
-    $Reponse_B = stripslashes($_REQUEST["Reponse_B"]);
-    $Reponse_B = mysqli_real_escape_string($conn, $Reponse_B);
-
-    $Reponse_C = stripslashes($_REQUEST["Reponse_C"]);
-    $Reponse_C = mysqli_real_escape_string($conn, $Reponse_C);
-
-    $Reponse_D = stripslashes($_REQUEST["Reponse_D"]);
-    $Reponse_D = mysqli_real_escape_string($conn, $Reponse_D);
-
-    $Reponse_Quizz = stripslashes($_REQUEST["Reponse_Quizz"]);
-    $Reponse_Quizz = mysqli_real_escape_string($conn, $Reponse_Quizz);
-
-    $Difficulte_question = stripslashes($_REQUEST["Difficulte_question"]);
-    $Difficulte_question = mysqli_real_escape_string($conn, $Difficulte_question);
+?>
+<?php 
+  
 
 
-    //todo On créer une requete SQL  pour faire une insertion des données recuperé dans notre formulaire
-    $query = "INSERT into `jeu_quizz` (Intitule_question, Reponse_A, Reponse_B, Reponse_C, Reponse_D, Reponse_Quizz, Difficulte_question) 
-    VALUES ('$Intitule_question', '$Reponse_A', '$Reponse_B','$Reponse_C', '$Reponse_D', '$Reponse_Quizz', '$Difficulte_question')";
+  // On vérifie si les variables  sont définies dans la requête en utilisant la fonction "isset()"
+     if(isset($_REQUEST["Intitule_question"],$_REQUEST["Reponse_A"],$_REQUEST["Reponse_B"],$_REQUEST["Reponse_C"],$_REQUEST["Reponse_D"],$_REQUEST["Reponse_Quizz"],$_REQUEST["Difficulte_question"])){
+      
+  
+      //! On supprime les caractères d'échappement des variables avec la fonction "stripslashes() et on empêche les injections SQL avec la fonction "mysqli_real_escape_string"
+      $Intitule_question = stripslashes($_REQUEST["Intitule_question"]);
+      $Intitule_question = mysqli_real_escape_string($conn, $Intitule_question);
+  
+      $Reponse_A = stripslashes($_REQUEST["Reponse_A"]);
+      $Reponse_A = mysqli_real_escape_string($conn, $Reponse_A);
+  
+      $Reponse_B = stripslashes($_REQUEST["Reponse_B"]);
+      $Reponse_B = mysqli_real_escape_string($conn, $Reponse_B);
+  
+      $Reponse_C = stripslashes($_REQUEST["Reponse_C"]);
+      $Reponse_C = mysqli_real_escape_string($conn, $Reponse_C);
+  
+      $Reponse_D = stripslashes($_REQUEST["Reponse_D"]);
+      $Reponse_D = mysqli_real_escape_string($conn, $Reponse_D);
+  
+      $Reponse_Quizz = stripslashes($_REQUEST["Reponse_Quizz"]);
+      $Reponse_Quizz = mysqli_real_escape_string($conn, $Reponse_Quizz);
+  
+      $Difficulte_question = stripslashes($_REQUEST["Difficulte_question"]);
+      $Difficulte_question = mysqli_real_escape_string($conn, $Difficulte_question);
+  
+  
+      //todo On créer une requete SQL  pour faire une insertion des données recuperé dans notre formulaire
+      $query = "INSERT into `jeu_quizz` (Intitule_question, Reponse_A, Reponse_B, Reponse_C, Reponse_D, Reponse_Quizz, Difficulte_question) 
+      VALUES ('$Intitule_question', '$Reponse_A', '$Reponse_B','$Reponse_C', '$Reponse_D', '$Reponse_Quizz', '$Difficulte_question')";
+  
+  //todo On exécute la requête avec la fonction "mysqli_query()
+      $res = mysqli_query($conn, $query);
+  
+  
+      // Si l'insertion a réussi, il affiche un message de succès, sinon il affiche un message d'erreur
+      if($res){
+        echo "<div class='succes'>
+              <h3>La question a été inscrite avec succès </h3>
+        </div>";
+     } else {
+      echo "<div class='error'>
+              <h3>Impossible de créer la question, verifier le formulaire</h3>
+        </div>";
+     }}       
+  ?>
 
-//todo On exécute la requête avec la fonction "mysqli_query()
-    $res = mysqli_query($conn, $query);
+  <!-- Formulaire pour supprimer les questions -->
+  <form class="box" action="" method="post" name="Jeu_Quizz">
+    <h2 class="box-title">Suppression des questions</h2>
+    <input type="text" class="box-input" name="id_question" placeholder="Quelle ligne est à supprimer">
+    <input class="update-delete" type="submit" value="Supprimer la question" name="submit" class="connecter">
+  </form>
 
+  <?php
 
-    // Si l'insertion a réussi, il affiche un message de succès, sinon il affiche un message d'erreur
-    if($res){
-      echo "<div class='sucess'>
-            <h3>La question a été inscrite avec succès </h3>
-      </div>";
-   } else {
-    echo "<p>Erreur<\p>";
-   }}       
-// ?>
+//todo on vérifie si le champ "id_question" est défini 
+if(isset($_REQUEST["id_question"])){
+
+  //todo on récupère la valeur de ce champ et on empeche les injections SQL
+    $id_question = stripslashes($_REQUEST["id_question"]);
+    $id_question = mysqli_real_escape_string($conn, $id_question);
+
+    $query = "DELETE FROM `Jeu_Quizz`  WHERE id_question = $id_question";
+
+    //todo on exécute une requête de suppression sur la base de données, On supprime la ligne correspondant à l'ID de la question spécifié dans le champ de texte.
+    $reponse2 = mysqli_query($conn, $query);
+}
+?>
+ <!-- Fin du formulaire de suppression -->
+ </div>
+ 
+<div class="tableauquiz">
+  
  <!-- FIN CREATION DE QUESTION -->
 
 
 <!-- Création d'un tableau HTML -->
   <table>
-    <tr>
-      <td>id_question</td>
-      <td>Intitule_question</td>
-      <td>Reponse_A</td>
-      <td>Reponse_B</td>
-      <td>Reponse_C</td>
-      <td>Reponse_D</td>
+    <tr class="titrecolonne">
+      <td>ID</td>
+      <td>Question</td>
+      <td>Rep A</td>
+      <td>Rep B</td>
+      <td>Rep C</td>
+      <td>Rep D</td>
       <td>Reponse_Quizz</td>
-      <td>Difficulte_question</td>
+      <td>Difficulté</td>
     </tr>
     <?php 
 
@@ -147,31 +193,11 @@ if ($conn->connect_error) {
     ?>
   </table>
 <!-- Fin du tableau -->
+</div>
+</section>
 
-<!-- Formulaire pour supprimer les questions -->
-  <form class="box" action="" method="post" name="Jeu_Quizz">
-    <h2 class="box-title">Suppression des questions</h2>
-    <input type="text" class="box-input" name="id_question" placeholder="Quelle ligne est à supprimer">
-    <input type="submit" value="Supprimer" name="submit" class="connecter">
-  </form>
 
-  <?php
-
-//todo on vérifie si le champ "id_question" est défini 
-if(isset($_REQUEST["id_question"])){
-
-  //todo on récupère la valeur de ce champ et on empeche les injections SQL
-    $id_question = stripslashes($_REQUEST["id_question"]);
-    $id_question = mysqli_real_escape_string($conn, $id_question);
-
-    $query = "DELETE FROM `Jeu_Quizz`  WHERE id_question = $id_question";
-
-    //todo on exécute une requête de suppression sur la base de données, On supprime la ligne correspondant à l'ID de la question spécifié dans le champ de texte.
-    $reponse2 = mysqli_query($conn, $query);
-}
-?>
- <!-- Fin du formulaire de suppression -->
-
+<section class="update_quizz">
  <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -179,9 +205,9 @@ error_reporting(E_ALL);
 
 // Affichage du formulaire pour sélectionner une question à modifier
 echo '<form class="box" action="" method="post" name="Jeu_Quizz">';
-echo '<h2 class="box-title">Mise à jour des questions</h2>';
+echo '<h2 class="box-title">Choisir une question à mettre à jour</h2>';
 echo '<input type="text" class="box-input" name="id_question2" placeholder="Quelle ligne est à mettre à jour">';
-echo '<input type="submit" value="Sélectionnez" name="submit" class="connecter">';
+echo '<input class="id_question" type="submit" value="Sélectionnez" name="submit" class="connecter">';
 echo '</form>';
 
 
@@ -208,22 +234,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_question2'])){
             $difficulte = $row['Difficulte_question'];
 
             // Affichage du formulaire de modification avec les valeurs existantes
-            echo '<form method="POST" action="#" name="editForm">';
+            echo '<form method="POST" action="#" name="editForm" class="updateform">';
+            echo '<h2 class="box-title">Mettre à jour la question</h2>';
             echo '<input type="hidden" name="id" value="' . $id . '">';
-            echo 'Intitule_question: <input type="text" name="intitule" value="' . $intitule .'"><br>';
-            echo 'Reponse_A: <input type="text" name="reponseA" value="' . $reponseA . '"><br>';
-            echo 'Reponse_B: <input type="text" name="reponseB" value="' . $reponseB . '"><br>';
-            echo 'Reponse_C: <input type="text" name="reponseC" value="' . $reponseC . '"><br>';
-            echo 'Reponse_D: <input type="text" name="reponseD" value="' . $reponseD . '"><br>';
-            echo 'Reponse_Quizz: <input type="text" name="reponseQuizz" value="' . $reponseQuizz . '"><br>';
-            echo 'Difficulte_question: <input type="text" name="difficulte" value="' . $difficulte . '"><br>';
-            echo '<input type="submit" value="Modifier">';
+            echo '<label>Intitulé de la question : </label>
+            <input type="text" name="intitule" value="' . $intitule .'"><br>';
+            echo '<label>Reponse A : </label>
+            <input type="text" class="box-reponse" name="reponseA" value="' . $reponseA . '"><br>';
+            echo '<label>Reponse B : </label>
+            <input type="text" class="box-reponse" name="reponseB" value="' . $reponseB . '"><br>';
+            echo '<label>Reponse C : </label>
+            <input type="text" class="box-reponse" name="reponseC" value="' . $reponseC . '"><br>';
+            echo '<label>Reponse D : </label>
+            <input type="text" class="box-reponse" name="reponseD" value="' . $reponseD . '"><br>';
+            echo '<label>Reponse QUIZZ : </label>
+            <input type="text" class="box-reponse" name="reponseQuizz" value="' . $reponseQuizz . '"><br>';
+            echo '<label>Difficultée : </label><input type="text" class="box-reponse" name="difficulte" value="' . $difficulte . '"><br>';
+            echo '<input type="submit" value="Modifier" class="id_question">';
             echo '</form>';
         } else {
-            echo "Aucun enregistrement trouvé avec cet ID.";
+            echo "<div class='error'>
+        <h3>Aucun enregistrement trouvé avec cet ID.</h3>
+  </div>";
         }
     } else {
-        echo "Erreur : ID manquant dans la soumission du formulaire.";
+        echo "<div class='error'>
+        <h3>Erreur : ID manquant dans la soumission du formulaire.</h3>
+  </div>";
     }
 }
 
@@ -244,24 +281,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $stmt->bind_param("sssssssi", $intitule, $reponseA, $reponseB, $reponseC, $reponseD, $reponseQuizz, $difficulte, $id);
 
       if ($stmt->execute()) {
-          echo "Enregistrement mis à jour avec succès.";
+          echo "<div class='succes'>
+              <h3>Enregistrement mis à jour avec succès. </h3>
+        </div>";
       } else {
-          echo "Erreur lors de la mise à jour de l'enregistrement : " . $stmt->error;
+        echo "<div class='error'>
+        <h3>Erreur lors de la mise à jour de l'enregistrement : </h3>
+  </div>" . $stmt->error;
+        
       }
-  } else {
-      echo "Erreur : Données du formulaire manquantes.";
-  }
+  } 
+  // else {
+  //     echo "Erreur : Données du formulaire manquantes.";
+  // }
 }
-
-
-
-
 ?>
-
-
 <!-- Fin du formulaire UPDATE -->
+</section>
 
 
 
 
 </body>
+</html>
