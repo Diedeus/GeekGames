7d2c6159515082288543af3d41a118c533d42032
= <palerm39@gmail.com>
2023-05-24 16:12:12 +0200

    3e commit du 24 05

--- Jeux/Juste_Prix/Script/justeprix.js
+++ Jeux/Juste_Prix/Script/justeprix.js
@@ -14,6 +14,57 @@ commencer.addEventListener("click", function(){
 let submit = document.querySelector('#btt')
 const valeur = parseInt(document.querySelector("#prix").value);
 let y = 0
+const inputfield = document.querySelector("#prix")
+
+inputfield.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault
+        const valeur = parseInt(document.querySelector("#prix").value); 
+        let newp = document.createElement('p')          //! création automatique de balise "p" pour ne pas limiter le nombre de réponse disponible
+        let r = document.querySelector(".reponse")      //! création automatique de balise "p" pour ne pas limiter le nombre de réponse disponible
+        console.log(valeur)
+        let compteur = 0
+        compteur += 1
+        
+    
+        
+    
+        if(valeur < Reponse_Jprix) {
+            
+            newp.textContent = "Votre réponse est en dessous du résultat attendu ==>" + valeur 
+            newp.style.backgroundColor="#ff0000"
+            y = ((Reponse_Jprix - valeur)*100)/Reponse_Jprix;
+            console.log(y)
+            barrechofroi(y)
+            
+            
+    
+        } else if (valeur > Reponse_Jprix) {
+            
+            newp.textContent = "votre réponse est au dessus du résultat attendu ==>" + valeur
+            newp.style.backgroundColor="#ff8400"
+            y = ((Math.abs( Reponse_Jprix - valeur ))*100)/Reponse_Jprix;
+            barrechofroi(y)
+            
+    
+        } else if (valeur == Reponse_Jprix) {
+           
+           newp.textContent = "WOW gg bro, tu as trouver la réponse ==> " + valeur
+           newp.style.backgroundColor="#20de06"
+           y = Reponse_Jprix - valeur;
+           barrechofroi(y)
+           let score = 2000
+           let scorefinal = 0
+           let valeurCacheeInput = document.querySelector("#valeurCacheeInput");
+           scorefinal = score - ( 50 * compteur )
+           console.log(scorefinal)
+           valeurCacheeInput.value = scorefinal;
+           
+           
+        }
+        r.prepend(newp) //! création automatique de balise "p" pour ne pas limiter le nombre de réponse disponible   
+    }
+})
 
 
 submit.addEventListener("click", function(e){
--- Jeux/Juste_Prix/justeprix.php
+++ Jeux/Juste_Prix/justeprix.php
@@ -6,27 +6,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le Juste Prix</title>
     <link rel="stylesheet" href="./Style/justeprix.css">
-    <script src="./Script/L'juste prix.js" defer></script>
+    
 </head>
 <body>
 
-<header>
-    <Section class="corps">
-        <h1>Le Juste Prix</h1>
+<?php 
+
+require('../../Registration/config.php');
+session_save_path("../../tmp");
+session_start();
+
+?>
+
+    <header>
+        <section class="accueil corps">
+            <h1>Bienvenue au Juste Prix</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium sapiente sint recusandae dolorum ea minus officia ab dolor laboriosam aliquam dolore beatae mollitia reprehenderit, debitis placeat, cumque iusto molestias aperiam.</p>
+            <button id="comm">Commencer</button>
+        </section>
+
+        <section class="jeujp corps">
+            <?php
+                $query = "SELECT Element_Jprix, Reponse_Jprix FROM Jeu_Jprix ORDER BY RAND() LIMIT 1";
+                $result = mysqli_query($conn, $query);
+               
+
+                if ($result->num_rows > 0) {
+                    $question = $result->fetch_assoc();
+                    $_SESSION['Element_Jprix'] = $question['Element_Jprix'];
+                    $_SESSION['Reponse_Jprix'] = $question['Reponse_Jprix'];
+
+                }
+            ?>
+            <script>
+                    let Img_Jprix = <?php echo json_encode($_SESSION['Element_Jprix']); ?>;
+                    let Reponse_Jprix = <?php echo json_encode($_SESSION['Reponse_Jprix']); ?>;
+            </script>
+
+            <h1>Le Juste Prix</h1>
             <input type="number" id="prix" placeholder="Proposez votre prix :">
-        <button type="submit" id="btt">Vérifier</button>
-        <span class="chofroi"><span id="tou"></span></span>
-    </Section>
-    <div class="right">
+            <button type="submit" id="btt">Vérifier</button>
+            <span class="chofroi"><span id="tou"></span></span>
+
+        </section>
+                    
+        <div class="right">
         <section class="imageprix">
 
         </section>
-    <section class="reponse">
+        <section class="reponse">
         
-    </section>
-    </div>
-    <div class="navinscri">
-    <nav class="home">
+        </section>
+        </div>   
+
+
+
+        <div class="navinscri">
+        <nav class="home">
             <ul>
                 <li><a href="../../index.php">MINI JEUX</a></li>
                 <li><a href="#">CREDITS</a></li>
@@ -42,8 +78,25 @@
         </div>
         <div class="clear"></div>
         <span class="barre"></span>
-    </div>
+        </div>
+
+       
+
 
     </header>
+    <form id="myForm" method="post">
+        <input type="hidden" name="scorefinal" id="valeurCacheeInput">
+        <input type="submit" value="Enregistrer votre Score" style="z-index:10">
+        </form>
+
+        <?php
+        if(isset($_POST['scorefinal'])){
+            $scorefinal = $_POST['scorefinal'];
+            $datescore = date('Y-m-d');
+            $id = $_SESSION['id_users'];
+            $query = "INSERT into `Score_Jprix`(Score_Jprix, Date_ScJprix, id_users) VALUES ('$scorefinal', '$datescore', '$id')";
+            $result = $conn->query($query);
+        }
+        ?>
+    <script src="../Juste_Prix/Script/justeprix.js"></script>
 </body>
-</html>
